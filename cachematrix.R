5a005095772387acbe6085dee2e8e66cda7a9ecf
JRD26 <91048299+JRD26@users.noreply.github.com>
2021-09-22 21:27:01 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
 ## There are two functions presented in this script: The first one is the makeCacheMatrix and the second one is the cacheSolve. ##
 
-## The first function creates a special "matrix" object that can cache its inverse.
+## The first function below creates a special "matrix" object that can cache its inverse. ##
 
-makeCacheMatrix <- function(x = matrix()) {
-  
+makeCacheMatrix <- function(x = matrix()) { 
+  ## This creates a matrix ##
 }
 
-## The second function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## The second function computes the inverse of the special "matrix" returned by makeCacheMatrix above. ##
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -14,26 +14,34 @@ cacheSolve <- function(x, ...) {
 
 ## Your assignment is to write a pair of functions that cache the inverse of a matrix. ##
 
-makeCacheMatrix <- function(a = matrix()) {
-  inv <- NULL
-  set <- function(b){ ## Set the value of the matrix ##
-    a <<- b
-    inv <<- NULL
+## R script for Programming Assignment 2: Lexical Scoping ##
+
+## 1st function ##
+
+makeCacheMatrix <- function(JRD = matrix()) {
+  invrsvrbl <- NULL
+  set1 <- function(DMSDS) {                                    ## Set the value of the matrix ##
+          JRD <<- DMSDS
+          invrsvrbl <<- NULL
   }
-  get <- function() {a}  ## Get the value of the matrix ##
-  set_Inverse <- function(inverse) {inv <<- inverse} ## Set the value of the Inverse ##
-  get_Inverse <- function() {inv} ## Get the value of the Inverse ##
-  list(set = set, get = get, set_Inverse = set_Inverse, get_Inverse = get_Inverse)
+  get1 <- function() {JRD}                                    ## Get the value of the matrix ##
+  set_Inverse2 <- function(inverse) {invrsvrbl <<- inverse}   ## Set the value of the Inverse ##
+  get_Inverse2 <- function() {invrsvrbl}                      ## Get the value of the Inverse ##
+  list(set1 = set1, get1 = get1, set_Inverse2 = set_Inverse2, get_Inverse2 = get_Inverse2)
 }
 
-cacheSolve <- function(a, ...) { ## Second function
-  inv <- a$get_Inverse()
-  if (!is.null(inv)) {
-    message("getting cached data") ## This should be displayed after running the cacheSolve function
-    return(inv)
+## 2nd function ##
+
+cacheSolve <- function(JRD, ...) { 
+  invrsvrbl <- JRD$get_Inverse2()
+  if (!is.null(invrsvrbl)) {
+    message("getting cached data") ## This should be displayed after running the cacheSolve function ##
+    return(invrsvrbl)
   }
-  matrix <- a$get()
-  inv <- solve(matrix, ...)
-  a$set_Inverse(inv)
-  inv 
+  matrix <- JRD$get1()
+  invrsvrbl <- solve(matrix, ...) ## To compute for the inverse of a square matrix ##
+  JRD$set_Inverse2(invrsvrbl)
+  invrsvrbl 
+  ## Check the results ##
+  ## Use 1:4 matrix ##
 }
